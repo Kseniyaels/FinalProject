6d0fc22e4db9bd8652aecfc5c1a825bd4383ca81
Kseniya Elsukova <kseniyaels@yandex.ru>
2023-01-29 22:23:37 +0300

    Задача

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string[]array = {"Hello", "2", "world", ":-)"};
+int finalArraySize = 0;
+int finalArrayIndex = 0;
+
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length <= 3) finalArraySize++;
+
+string[] finalArray = new string[finalArraySize];
+
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length <= 3) finalArray[finalArrayIndex++] = array[i];
+
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < array.Length; i++)
+Console.WriteLine(array[i] + ((i != array.Length-1)? ", ": ""));
+Console.WriteLine();
+
+Console.WriteLine("Измененный массив: ");
+for (int i = 0; i < finalArray.Length; i++)
+Console.WriteLine(finalArray[i] + ((i != finalArray.Length-1)? ", ": ""));
